ad1f342fa4c89b5090390b9f48fc78c241ce06f5
Sebastian Kawelke <kawelkesebastian@gmail.com>
2023-07-22 11:47:47 +0200

    Worked on dashboard - stats ui

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -23,6 +23,7 @@ import {
   ServerIcon,
   SignalIcon,
   XMarkIcon,
+  RectangleGroupIcon,
 } from "@heroicons/react/24/outline";
 import {
   Bars3Icon,
@@ -37,9 +38,11 @@ import MobileSidebar from "../Navigation/MobileSidebar";
 import Sidebar from "../Navigation/Sidebar";
 import { ActivityItem, ActivityItems } from "@/types/common";
 import { activityString, calculateActivityString } from "@/utils/activityFeed";
+import SingleStatGroup from "../SingleStatGroup";
 
 const navigation = [
-  { name: "Projects", href: "#", icon: ServerIcon, current: true },
+  { name: "Dashboard", href: "#", icon: RectangleGroupIcon, current: true },
+  { name: "Projects", href: "#", icon: ServerIcon, current: false },
   { name: "Latest Activity", href: "#", icon: SignalIcon, current: false },
   { name: "Domains", href: "#", icon: GlobeAltIcon, current: false },
   { name: "Reports", href: "#", icon: ChartBarSquareIcon, current: false },
@@ -194,127 +197,13 @@ export default function Example() {
 
           <main className="lg:pr-96">
             <header className="flex items-center justify-between border-b border-white/5 px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
-              <h1 className="text-base font-semibold leading-7 text-white">
-                Projects
+              <h1 className="text-lg font-semibold leading-7 text-white">
+                Dashboard
               </h1>
-
-              {/* Sort dropdown */}
-              <Menu as="div" className="relative">
-                <Menu.Button className="flex items-center gap-x-1 text-sm font-medium leading-6 text-white">
-                  Sort by
-                  <ChevronUpDownIcon
-                    className="h-5 w-5 text-gray-500"
-                    aria-hidden="true"
-                  />
-                </Menu.Button>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="absolute right-0 z-10 mt-2.5 w-40 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active ? "bg-gray-50" : "",
-                            "block px-3 py-1 text-sm leading-6 text-gray-900",
-                          )}
-                        >
-                          Name
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active ? "bg-gray-50" : "",
-                            "block px-3 py-1 text-sm leading-6 text-gray-900",
-                          )}
-                        >
-                          Date updated
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active ? "bg-gray-50" : "",
-                            "block px-3 py-1 text-sm leading-6 text-gray-900",
-                          )}
-                        >
-                          Environment
-                        </a>
-                      )}
-                    </Menu.Item>
-                  </Menu.Items>
-                </Transition>
-              </Menu>
             </header>
-
-            {/* Deployment list */}
-            <ul role="list" className="divide-y divide-white/5">
-              {projects.map((project) => (
-                <li
-                  key={project.id}
-                  className="relative flex items-center space-x-4 px-4 py-4 sm:px-6 lg:px-8"
-                >
-                  <div className="min-w-0 flex-auto">
-                    <div className="flex items-center gap-x-3">
-                      <div
-                        className={classNames(
-                          statuses[project.status],
-                          "flex-none rounded-full p-1",
-                        )}
-                      >
-                        <div className="h-2 w-2 rounded-full bg-current" />
-                      </div>
-                      <h2 className="min-w-0 text-sm font-semibold leading-6 text-white">
-                        <a href={project.href} className="flex gap-x-2">
-                          <span className="truncate">{project.teamName}</span>
-                          <span className="text-gray-400">/</span>
-                          <span className="whitespace-nowrap">
-                            {project.projectName}
-                          </span>
-                          <span className="absolute inset-0" />
-                        </a>
-                      </h2>
-                    </div>
-                    <div className="mt-3 flex items-center gap-x-2.5 text-xs leading-5 text-gray-400">
-                      <p className="truncate">{project.description}</p>
-                      <svg
-                        viewBox="0 0 2 2"
-                        className="h-0.5 w-0.5 flex-none fill-gray-300"
-                      >
-                        <circle cx={1} cy={1} r={1} />
-                      </svg>
-                      <p className="whitespace-nowrap">{project.statusText}</p>
-                    </div>
-                  </div>
-                  <div
-                    className={classNames(
-                      environments[project.environment],
-                      "flex-none rounded-full px-2 py-1 text-xs font-medium ring-1 ring-inset",
-                    )}
-                  >
-                    {project.environment}
-                  </div>
-                  <ChevronRightIcon
-                    className="h-5 w-5 flex-none text-gray-400"
-                    aria-hidden="true"
-                  />
-                </li>
-              ))}
-            </ul>
+            <div className="px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
+              <SingleStatGroup />
+            </div>
           </main>
 
           {/* Activity feed */}
