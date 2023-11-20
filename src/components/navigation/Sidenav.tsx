024bd4a7e021b40ac57c49215fe8083a565f044b
timbastin <bastin.tim@gmail.com>
2023-11-20 10:56:14 +0100

    adds project page

--- src/components/navigation/Sidenav.tsx
+++ src/components/navigation/Sidenav.tsx
@@ -112,7 +112,7 @@ export default function Sidenav({ navigation }: Props) {
         <div className="flex h-24 shrink-0 items-center">
           <Image
             className="mt-6 h-10 w-auto"
-            src="logo_flaw_fix_white_l3.svg"
+            src="/logo_flaw_fix_white_l3.svg"
             alt="FlawFix by l3montree Logo"
             width={32}
             height={32}
--- src/pages/[organizationSlug]/projects/[projectSlug]/index.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/index.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { GetServerSidePropsContext } from "next";
+import { FunctionComponent } from "react";
+import Page from "../../../../components/Page";
+import { withInitialState } from "../../../../decorators/withInitialState";
+import { withSession } from "../../../../decorators/withSession";
+import { getApiClientFromContext } from "../../../../services/flawFixApi";
+import { ProjectDTO } from "../../../../types/api";
 
-const Index = () => {
-  return <div>Hallo</div>;
+interface Props {
+  project: ProjectDTO;
+}
+const Index: FunctionComponent<Props> = ({ project }) => {
+  return <Page title={project.name}>Hallo</Page>;
 };
 
+export const getServerSideProps = withSession(
+  withInitialState(async (context: GetServerSidePropsContext) => {
+    // fetch the project
+    const { organizationSlug, projectSlug } = context.params!;
+    const apiClient = getApiClientFromContext(context);
+    const resp = await apiClient(
+      "/organizations/" + organizationSlug + "/projects/" + projectSlug + "/",
+    );
+
+    return {
+      props: {
+        project: await resp.json(),
+      },
+    };
+  }),
+);
 export default Index;
