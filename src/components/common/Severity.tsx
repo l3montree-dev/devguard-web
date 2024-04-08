e2095dc58a2d506979330483e23fe24a96f51bc0
timbastin <bastin.tim@gmail.com>
2024-04-08 13:00:56 +0200

    fixes lint errors

--- src/components/common/Severity.tsx
+++ src/components/common/Severity.tsx
@@ -1,16 +1,21 @@
 import { classNames } from "@/utils/common";
 
+const getClassNames = (severity: string) => {
+  switch (severity) {
+    case "CRITICAL":
+      return "bg-red-200 text-red-700 border border-red-300";
+    case "HIGH":
+      return "bg-orange-200 text-orange-700 border border-red-300";
+    case "MEDIUM":
+      return "bg-yellow-300 border border-yellow-500 text-yellow-900";
+    case "LOW":
+      return "text-green-700 border border-green-400 bg-green-200";
+    default:
+      return "text-white bg-slate-500";
+  }
+};
 const Severity = ({ severity }: { severity: string }) => {
-  const cls =
-    severity === "CRITICAL"
-      ? "bg-red-200 text-red-700 border border-red-300"
-      : severity === "HIGH"
-      ? "bg-orange-200 text-orange-700 border border-red-300"
-      : severity === "MEDIUM"
-      ? "bg-yellow-300 border border-yellow-500 text-yellow-900"
-      : severity === "LOW"
-      ? "text-green-700 border border-green-400 bg-green-200"
-      : "text-white bg-slate-500";
+  const cls = getClassNames(severity);
 
   return (
     <div className="flex">
