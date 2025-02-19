4d45f716d04baf442f81910b415e6510c2ab291b
Lars Hermges <lschwan43@gmail.com>
2025-02-19 19:38:22 +0100

    json now gets printed, now I have to check and send to patrick, also onload is a lie!

--- src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/index.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/index.tsx
@@ -57,17 +57,13 @@ function DropzoneComponent() {
   const onDrop = useCallback((acceptedFiles: File[]) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader();
-
-      const test: any = JSON.parse(this.any);
-      console.log(test.bomFormat);
       reader.onabort = () => console.log("file reading was aborted");
       reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
-        // Do whatever you want with the file contents
         const binaryStr = reader.result;
         console.log(binaryStr);
       };
-      reader.readAsArrayBuffer(file);
+      reader.readAsText(file);
     });
   }, []);
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
@@ -75,7 +71,7 @@ function DropzoneComponent() {
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
-      <p>test test 2</p>
+      <p>click me</p>
     </div>
   );
 }
