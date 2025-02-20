a5766dd861a412233b39405e27b3a08280a0a338
Rafi <refaei.shikho@hotmail.com>
2025-02-20 17:07:46 +0100

    fix risks display when no risk
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

--- src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/risk-handling.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/risk-handling.tsx
@@ -229,26 +229,10 @@ const Index: FunctionComponent<Props> = (props) => {
       }, 500),
     [router],
   );
-  let activeScan = false;
-  if (props.exists) {
-    activeScan = Boolean(asset?.lastContainerScan ?? asset?.lastScaScan);
-  }
+
   return (
     <Page Menu={assetMenu} title={"Risk Handling"} Title={<AssetTitle />}>
       {!props.exists ? (
-        <EmptyParty
-          title="No identified risks"
-          description="No identified risks for this asset."
-        />
-      ) : activeScan &&
-        table.getRowCount() === 0 &&
-        Object.keys(router.query).length === 4 ? (
-        <EmptyParty
-          title="No identified risks"
-          description="No identified risks for this asset."
-        />
-      ) : /**  the query will contain organizationSlug, projectSlug and assetSlug - thus 3 is empty  */
-      table.getRowCount() === 0 && Object.keys(router.query).length === 4 ? (
         <EmptyList
           title="You do not have any identified risks for this asset."
           description="Risk identification is the process of determining what risks exist in the asset and what their characteristics are. This process is done by identifying, assessing, and prioritizing risks."
@@ -266,6 +250,16 @@ const Index: FunctionComponent<Props> = (props) => {
             </Link>
           }
         />
+      ) : table.getRowCount() === 0 &&
+        Object.keys(router.query).length === 4 ? (
+        <>
+          <BranchTagSelector branches={branches} tags={tags} />
+
+          <EmptyParty
+            title="No identified risks"
+            description="No identified risks for this asset."
+          />
+        </>
       ) : (
         <div>
           <BranchTagSelector branches={branches} tags={tags} />
