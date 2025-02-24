e29d20c16b22717dac14606f83b56524537afaae
Lars Hermges <lschwan43@gmail.com>
2025-02-24 10:21:45 +0100

    updated to make it react friendly, conversion to string is wonky, and should be revised at some point, checks for formats should work nowm, yippie

--- src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/index.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/index.tsx
@@ -77,38 +77,37 @@ const handleSBOM = async (text: string) => {
 };
 
 function DropzoneComponent() {
-  let myEl;
+  const [fileContent, setFileContent] = useState<string>("");
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader();
       reader.onabort = () => console.log("file reading was aborted");
       reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
-        const binaryStr = reader.result;
-        console.log(binaryStr);
-      };
+        const text = reader.result as string;
+        const sbomparse = JSON.parse(text);
+        setFileContent(text);
+        //console.log(text);
 
+        if (sbomparse.bomFormat && sbomparse.specVersion === "1.6") {
+          console.log("yay it works");
+        }
+      };
       reader.readAsText(file);
-      //console.log(file.text().then());
-      const text = reader.result;
-      myEl = JSON.stringify(text) as string;
-      //console.log(test);
-      //console.log(acceptedFiles);
-      //acceptedFiles.toString();
-      //JSON.parse(text as string);
-      //acceptedfile ist aufjedenfall die ganze datei mit ihrem inhalt, die muss also mit .text geparsed
     });
   }, []);
+
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    accept: { "application/json": ["./json"] },
+    accept: { "application/json": [".json"] },
   });
 
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
       <p>click me</p>
-      <Button onClick={() => handleSBOM(myEl)}> Hey</Button>
+      <Button onClick={() => handleSBOM(fileContent)}> Hey</Button>
     </div>
   );
 }
@@ -128,12 +127,6 @@ const SecurityControlCenter: FunctionComponent<Props> = () => {
   ) as PatWithPrivKey | null;
 
   const { isLoading, handleAutosetup, progress, Loader } = useAutosetup("full");
-
-  useDropzone({
-    accept: {
-      "application/json": [".json"],
-    },
-  });
 
   return (
     <>
