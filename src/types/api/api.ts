785afb91997ebcef9aa1467a9ec4a742f1495958
Rafi <refaei.shikho@hotmail.com>
2025-03-10 10:31:57 +0100

    add oldRisk property to RiskCalculationReport
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

--- src/types/api/api.ts
+++ src/types/api/api.ts
@@ -176,6 +176,7 @@ export interface RiskCalculationReport {
   integrityRequirement: string;
   availabilityRequirement: string;
   risk: number;
+  oldRisk: number;
 }
 
 interface BaseFlawEventDTO {
