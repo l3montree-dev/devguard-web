226011317a73142f0ea696cb3e4d8e2e7394e35e
Lars <lschwan43@gmail.com>
2025-03-24 12:12:14 +0100

    weird input

--- src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/settings.test.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/settings.test.tsx
@@ -3,10 +3,15 @@ import { isNumber } from "./settings";
 describe("input test, to check if it always return true, when a number is given", () => {
   it("should only return true if its a number ", () => {
     const table = [1, 2, 3, 4, 5, 6];
+    const table2 = [NaN, null, "string", undefined];
 
     for (const index of table) {
       const actual = isNumber(index);
       expect(actual).toBe(true);
+    }
+    for (const index of table2) {
+      const actual = isNumber(index);
+      expect(actual).toBe(false);
     }
   });
 });
