328bdbbf37ba67afb55d2e4e3cbeba9bfecd2383
Lars <lschwan43@gmail.com>
2025-03-24 12:02:40 +0100

    not very complicated I will now add edge cases

--- src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/settings.test.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/settings.test.tsx
@@ -2,18 +2,11 @@ import { isNumber } from "./settings";
 
 describe("input test, to check if it always return true, when a number is given", () => {
   it("should only return true if its a number ", () => {
-    const table = {
-      1: true,
-      2: true,
-      3: true,
-      undefined: false,
-      NaN: false,
-    };
+    const table = [1, 2, 3, 4, 5, 6];
 
-    for (const index of Object.entries(table)) {
-      const actual = isNumber(index[0]);
-
-      expect(actual).toBe(index[1]);
+    for (const index of table) {
+      const actual = isNumber(index);
+      expect(actual).toBe(true);
     }
   });
 });
