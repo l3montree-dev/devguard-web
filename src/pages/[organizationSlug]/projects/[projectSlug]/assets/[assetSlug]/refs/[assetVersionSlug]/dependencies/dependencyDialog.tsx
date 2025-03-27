0104c8a4d5cef6a0b9c136851c24dfafc0436dd6
Lars <lschwan43@gmail.com>
2025-03-27 18:18:59 +0100

    DependencyGraph now gets data, which is still not rendering yet

--- src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/dependencies/dependencyDialog.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/dependencies/dependencyDialog.tsx
@@ -1,12 +1,15 @@
 import { useRouter } from "next/router";
-
-import { config } from "@/config";
-import { Dispatch, FunctionComponent, SetStateAction, useEffect } from "react";
+import {
+  Dispatch,
+  FunctionComponent,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -17,15 +20,13 @@ import { useActiveOrg } from "@/hooks/useActiveOrg";
 import { useActiveProject } from "@/hooks/useActiveProject";
 import { browserApiClient } from "@/services/devGuardApi";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 interface Props {
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
   data: any;
 }
-// hier müssen ins interface dann noch die anderen daten von row.row.* rein, die kann ich dann bei onClick denke ich mal auch mitgeben in useref maybe?
-
-// localhost:8080/api/v1/organizations/bizarreorganization/projects/adventurousproject/assets/joasset/refs/main/path-to-component/?scanner=SBOM-File-Upload&purl=pkg%3Anpm%2Feslint-module-utils%402.11.1
 
 const DependencyDialog: FunctionComponent<Props> = ({
   open,
@@ -37,6 +38,8 @@ const DependencyDialog: FunctionComponent<Props> = ({
   const organization = useActiveOrg();
   const project = useActiveProject();
 
+  const [graphData, setGraphData] = useState<any>(null);
+
   const handleGraphFetch = async (data: string) => {
     const resp = await browserApiClient(
       `/organizations/${organization.slug}/projects/${project.slug}/assets/${asset?.slug}/refs/main/path-to-component/?scanner=SBOM-File-Upload&purl=${encodeURIComponent(data)}`,
@@ -44,27 +47,40 @@ const DependencyDialog: FunctionComponent<Props> = ({
         method: "GET",
       },
     );
+
+    if (resp.ok) {
+      const json2 = await resp.json();
+      setGraphData(json2);
+    } else {
+      toast.error("Could not fetch Graph Data from Endpoint");
+    }
   };
 
   return (
     <Dialog open={open}>
       <DialogContent setOpen={setOpen}>
         <DialogHeader>
-          <DialogTitle>reason {data.reason}</DialogTitle>
-          <DialogTitle>details {data.details}</DialogTitle>
-          <DialogTitle>name {data.name}</DialogTitle>
-          <DialogTitle>score {data.score}</DialogTitle>
-          <DialogTitle>shortDescription {data.shortDescription}</DialogTitle>
-          <DialogTitle>dependencyPurl {data.purl}</DialogTitle>
+          <DialogTitle>reason: {data.reason}</DialogTitle>
+          <DialogTitle>details: {data.details}</DialogTitle>
+          <DialogTitle>name: {data.name}</DialogTitle>
+          <DialogTitle>score: {data.score}</DialogTitle>
+          <DialogTitle>shortDescription: {data.shortDescription}</DialogTitle>
+          <DialogTitle>dependencyPurl: {data.purl}</DialogTitle>
         </DialogHeader>
-        <Button onClick={() => handleGraphFetch(data.purl)}></Button>
+
+        <Button onClick={() => handleGraphFetch(data.purl)}>Fetch Graph</Button>
+
         <hr />
-        {/* <DependencyGraph
-          width={100}
-          height={100}
-          flaws={}
-          graph={}
-        ></DependencyGraph> */}
+
+        {graphData && (
+          <DependencyGraph
+            width={100}
+            height={100}
+            flaws={[]}
+            graph={graphData}
+          />
+        )}
+
         <DialogFooter>
           <div>hi</div>
         </DialogFooter>
