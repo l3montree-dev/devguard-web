ec005a26bc5cabcaf2e3a00508811268d4a1ebc0
Lars <lschwan43@gmail.com>
2025-04-21 12:25:43 +0200

    customize own Podium

--- src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/dependencies/Podium.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/dependencies/Podium.tsx
@@ -1,77 +1,11 @@
-"use client";
-
-import { TrendingUp } from "lucide-react";
-import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  ChartConfig,
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "@/components/ui/chart";
-const chartData = [
-  {
-    month: "here is a long ass critria name that i-",
-    desktop: 1,
-  },
-  { month: "1st", desktop: 3 },
-  { month: "2nd", desktop: 2 },
-];
-
-const chartConfig = {
-  desktop: {
-    label: "fuck",
-    color: "hsl(var(--chart-1))",
-  },
-} satisfies ChartConfig;
-
 export function Podium() {
   return (
-    <div>
-      <CardHeader>
-        <CardTitle>Worst Categories</CardTitle>
-        <CardDescription>This sucks.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <ChartContainer config={chartConfig}>
-          <BarChart
-            accessibilityLayer
-            data={chartData}
-            margin={{
-              top: 30,
-            }}
-          >
-            <XAxis
-              dataKey="month"
-              tickLine={false}
-              tickMargin={10}
-              axisLine={false}
-            />
-
-            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8}>
-              <LabelList
-                position="top"
-                offset={12}
-                className="fill-foreground"
-                fontSize={12}
-              />
-            </Bar>
-          </BarChart>
-        </ChartContainer>
-      </CardContent>
-      <CardFooter className="flex-col items-start gap-2 text-sm">
-        <div className="leading-none text-muted-foreground">
-          3 Worst categories
-        </div>
-      </CardFooter>
+    <div className="flex items-center justify-center">
+      <div className=" flex grid-cols-3 space-x-5">
+        <div className="h-20 w-20 bg-gradient-to-b from-red-500"></div>
+        <div className="h-20 w-20 bg-gradient-to-b from-red-500"></div>
+        <div className="h-20 w-20 bg-current"></div>
+      </div>
     </div>
   );
 }
