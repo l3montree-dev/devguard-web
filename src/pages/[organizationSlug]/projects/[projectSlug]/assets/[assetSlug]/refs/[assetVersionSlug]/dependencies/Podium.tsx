4acfb19510f49e330d061205e14cadf9842114f7
Lars <lschwan43@gmail.com>
2025-04-21 11:47:18 +0200

    removed outline etc in the background

--- src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/dependencies/Podium.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/dependencies/Podium.tsx
@@ -18,27 +18,27 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 const chartData = [
-  { month: "January", desktop: 186 },
-  { month: "February", desktop: 305 },
-  { month: "March", desktop: 237 },
-  { month: "April", desktop: 73 },
-  { month: "May", desktop: 209 },
-  { month: "June", desktop: 214 },
+  {
+    month: "here is a long ass critria name that i-",
+    desktop: 1,
+  },
+  { month: "1st", desktop: 3 },
+  { month: "2nd", desktop: 2 },
 ];
 
 const chartConfig = {
   desktop: {
-    label: "Desktop",
+    label: "fuck",
     color: "hsl(var(--chart-1))",
   },
 } satisfies ChartConfig;
 
 export function Podium() {
   return (
-    <Card>
+    <div>
       <CardHeader>
-        <CardTitle>Bar Chart - Label</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle>Worst Categories</CardTitle>
+        <CardDescription>This sucks.</CardDescription>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
@@ -46,16 +46,14 @@ export function Podium() {
             accessibilityLayer
             data={chartData}
             margin={{
-              top: 20,
+              top: 30,
             }}
           >
-            <CartesianGrid vertical={false} />
             <XAxis
               dataKey="month"
               tickLine={false}
               tickMargin={10}
               axisLine={false}
-              tickFormatter={(value) => value.slice(0, 3)}
             />
 
             <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8}>
@@ -74,6 +72,6 @@ export function Podium() {
           3 Worst categories
         </div>
       </CardFooter>
-    </Card>
+    </div>
   );
 }
