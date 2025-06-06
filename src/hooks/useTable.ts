971d3651f70362010a72b6b90ea7c52e6674ea16
Lars <lschwan43@gmail.com>
2025-06-06 16:12:25 +0200

    Update useTable.ts
    Signed-off-by: Lars   <lschwan43@gmail.com>

--- src/hooks/useTable.ts
+++ src/hooks/useTable.ts
@@ -36,7 +36,6 @@ export default function useTable<T>({
       debounce((e: React.ChangeEvent<HTMLInputElement>) => {
         setIsLoading(true);
         // remove all sorting query params
-        console.log("meow");
         const params = router.query;
         if (e.target.value === "") {
           delete params["search"];
