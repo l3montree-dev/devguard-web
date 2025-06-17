94c6537aa45d736bf5e4f9be4bc850f395e54e70
Lars <lschwan43@gmail.com>
2025-06-17 11:13:19 +0200

    Compliance Grid now has no compliance rules too

--- src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/index.tsx
+++ src/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/refs/[assetVersionSlug]/index.tsx
@@ -197,6 +197,13 @@ const Index: FunctionComponent<Props> = ({
                     </div>
                   ))}
                 </div>
+              ) : failingControls.length === 0 ? (
+                <Badge variant={"outline"}>
+                  <CheckBadgeIcon className="-ml-2 h-8 w-8 text-gray-500" />
+                  <span className="pl-2 text-base">
+                    No compliance rules are activated
+                  </span>
+                </Badge>
               ) : (
                 <div className="flex flex-row items-center gap-2">
                   <Badge variant={"success"}>
