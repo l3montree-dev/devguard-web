775de6ea7f72b7db121292b8e74e136a48259a08
Lars <lschwan43@gmail.com>
2025-06-18 16:06:53 +0200

    works now

--- src/components/kratos/NodeInputCheckbox.tsx
+++ src/components/kratos/NodeInputCheckbox.tsx
@@ -15,8 +15,10 @@ export function NodeInputCheckbox<T>({
   // Render only if it's the confirmedTerms checkbox and disabled is true
   if (
     attributes.name === "traits.confirmedTerms" &&
-    attributes.value === false
+    attributes.value === true
   ) {
+    null;
+  } else
     return (
       <div className="flex items-center space-x-2">
         <Checkbox
@@ -29,7 +31,4 @@ export function NodeInputCheckbox<T>({
         <Label htmlFor={id}>{getNodeLabel(node)}</Label>
       </div>
     );
-  }
-
-  return null;
 }
