9099c392748f05db6e42292cb5df55cdf75a4009
Lars <lschwan43@gmail.com>
2025-06-24 15:23:22 +0200

    messages logic in ory is insane

--- src/components/kratos/Messages.tsx
+++ src/components/kratos/Messages.tsx
@@ -13,11 +13,48 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { UiText } from "@ory/client";
+import { UiNode, UiNodeTextAttributes, UiText } from "@ory/client";
 import Callout from "../common/Callout";
+
+const Content = ({ node, attributes }: Props) => {
+  switch (attributes.text.id) {
+    case 1050015:
+      // This text node contains lookup secrets. Let's make them a bit more beautiful!
+      const secrets = (attributes.text.context as any).secrets.map(
+        (text: UiText, k: number) => (
+          <div
+            key={k}
+            data-testid={`node/text/${attributes.id}/lookup_secret`}
+            className="col-xs-3"
+          >
+            {/* Used lookup_secret has ID 1050014 */}
+            <code>{text.id === 1050014 ? "Used" : text.text}</code>
+          </div>
+        ),
+      );
+      return (
+        <div
+          className="container-fluid mt-4 rounded-sm bg-white p-2 text-sm dark:bg-slate-800"
+          data-testid={`node/text/${attributes.id}/text`}
+        >
+          <div className="row">{secrets}</div>
+        </div>
+      );
+  }
+
+  return (
+    <div
+      className="mt-2 font-medium"
+      data-testid={`node/text/${attributes.id}/text`}
+    >
+      {attributes.text.text}
+    </div>
+  );
+};
 
 interface MessageProps {
   message: UiText;
+  attributes?: UiNodeTextAttributes;
 }
 
 const messageType2Intent = {
@@ -25,17 +62,23 @@ const messageType2Intent = {
   success: "success",
   info: "info",
 };
+
 export const Message = ({ message }: MessageProps) => {
-  return (
-    <>
-      <Callout intent="danger">
-        <div className="flex flex-row gap-4">
-          <p className="flex-1" data-testid={`ui/message/${message.id}`}>
-            {message.text}
-          </p>
-        </div>
+  const error = message.id === 1010016;
+  console.log("error", error);
+  if (error)
+    return (
+      <Callout intent="success">
+        <p className="text-sm" data-testid={`ui/message/${message.id}`}>
+          {message.text}
+        </p>
       </Callout>
-    </>
+    );
+
+  return (
+    <p className="text-sm" data-testid={`ui/message/${message.id}`}>
+      {message.text}
+    </p>
   );
 };
 
@@ -44,14 +87,25 @@ interface MessagesProps {
 }
 
 export const Messages = ({ messages }: MessagesProps) => {
+  messages?.map((message) => {
+    switch (message.id) {
+      case 1010016:
+        console.log("teststst!!!!!!!!!!!!");
+        return (
+          <>
+            <Message key={message.id} message={message} />
+          </>
+        );
+    }
+  });
+
   if (!messages) {
     // No messages? Do nothing.
     return null;
   }
-
   return (
     <>
-      {messages.map((message) => (
+      {messages?.map((message) => (
         <Message key={message.id} message={message} />
       ))}
     </>
