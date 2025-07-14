38996788f723ced0a589650a513d9b1083c13fdb
Sebastian Kawelke <sebastian.kawelke@l3montree.com>
2025-07-14 15:11:26 +0200

    Reduces error tracking data and setup
    Signed-off-by: Sebastian Kawelke <sebastian.kawelke@l3montree.com>

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,9 +4,30 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_ERROR_TRACKING_DSN,
+if (!process.env.NEXT_PUBLIC_ERROR_TRACKING_DSN) {
+  console.warn(
+    "Sentry is not initialized because ERROR_TRACKING_DSN is not set.",
+  );
+} else {
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_ERROR_TRACKING_DSN,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+
+    release: process.env.NEXT_PUBLIC_VERSION,
+
+    beforeSend(event) {
+      if (event.request && event.request.cookies) {
+        delete event.request.cookies;
+      }
+      // remove user ip from event
+      if (event.user && event.user.ip_address) {
+        delete event.user.ip_address;
+      }
+      return event;
+    },
+
+    sendDefaultPii: false,
+  });
+}
