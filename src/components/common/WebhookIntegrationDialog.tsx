49b402501fe3c0d548f2e15d1242a40d92570eef
Rafi <refaei.shikho@hotmail.com>
2025-07-24 17:09:47 +0200

    Enhance WebhookIntegrationDialog with new input features and reset logic
    Signed-off-by: Rafi <refaei.shikho@hotmail.com>

--- src/components/common/WebhookIntegrationDialog.tsx
+++ src/components/common/WebhookIntegrationDialog.tsx
@@ -2,7 +2,7 @@ import { useActiveOrg } from "@/hooks/useActiveOrg";
 import { useLoader } from "@/hooks/useLoader";
 import { browserApiClient } from "@/services/devGuardApi";
 import { WebhookDTO } from "@/types/api/api";
-import React, { FunctionComponent, ReactNode } from "react";
+import React, { FunctionComponent, ReactNode, useEffect } from "react";
 import { useForm, UseFormReturn } from "react-hook-form";
 import { toast } from "sonner";
 import { AsyncButton, Button } from "../ui/button";
@@ -27,6 +27,10 @@ import { urlToBaseURL } from "../../utils/url";
 import { Switch } from "../ui/switch";
 import { on } from "events";
 import { useActiveProject } from "@/hooks/useActiveProject";
+import { Card } from "../ui/card";
+import { InputWithButton } from "../ui/input-with-button";
+import { ClipboardDocumentIcon } from "@heroicons/react/24/outline";
+import { generateNewSecret } from "@/pages/[organizationSlug]/projects/[projectSlug]/assets/[assetSlug]/settings";
 
 interface Props {
   Button: ReactNode;
@@ -52,6 +56,23 @@ export const WebhookIntegrationDialog: FunctionComponent<Props> = ({
       vulnEnabled: false,
     },
   });
+
+  // Reset form values when initialValues change
+  useEffect(() => {
+    if (initialValues) {
+      form.reset(initialValues);
+    } else {
+      form.reset({
+        name: "",
+        description: "",
+        url: "",
+        secret: "",
+        sbomEnabled: false,
+        vulnEnabled: false,
+      });
+    }
+  }, [initialValues, form]);
+
   const { Loader, waitFor, isLoading } = useLoader();
   const activeOrg = useActiveOrg();
   const project = useActiveProject();
@@ -107,6 +128,11 @@ export const WebhookIntegrationDialog: FunctionComponent<Props> = ({
     }
   };
 
+  const handleGenerateNewSecret = () => {
+    const newSecret = generateNewSecret();
+    form.setValue("secret", newSecret);
+  };
+
   return (
     <Dialog onOpenChange={setOpen} open={open}>
       <DialogTrigger asChild>{Trigger}</DialogTrigger>
@@ -134,11 +160,7 @@ export const WebhookIntegrationDialog: FunctionComponent<Props> = ({
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input
-                      placeholder="My Webhook"
-                      {...field}
-                      defaultValue={initialValues?.name}
-                    />
+                    <Input placeholder="My Webhook" {...field} />
                   </FormControl>
                   <FormDescription>
                     A name to identify the webhook later on.
@@ -153,11 +175,7 @@ export const WebhookIntegrationDialog: FunctionComponent<Props> = ({
                 <FormItem>
                   <FormLabel>Description</FormLabel>
                   <FormControl>
-                    <Input
-                      placeholder="My Webhook"
-                      {...field}
-                      defaultValue={initialValues?.description}
-                    />
+                    <Input placeholder="My Webhook" {...field} />
                   </FormControl>
                   <FormDescription>
                     A description to identify the webhook later on.
@@ -178,12 +196,12 @@ export const WebhookIntegrationDialog: FunctionComponent<Props> = ({
                       type="url"
                       required
                       {...field}
-                      defaultValue={initialValues?.url}
                     />
                   </FormControl>
                   <FormDescription>
                     The URL to which DevGuard will send the webhook
-                    notifications.
+                    notifications. Ensure that the URL is publicly accessible
+                    and can receive POST requests from DevGuard.
                   </FormDescription>
                 </FormItem>
               )}
@@ -194,54 +212,74 @@ export const WebhookIntegrationDialog: FunctionComponent<Props> = ({
               name="secret"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Secret token </FormLabel>
                   <FormControl>
-                    <Input
-                      placeholder="my-secret"
+                    <InputWithButton
+                      label="Secret token"
+                      placeholder="the secret"
                       {...field}
-                      defaultValue={initialValues?.secret}
+                      copyable={true}
+                      SVG={
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-4"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      }
+                      onClick={handleGenerateNewSecret}
                     />
                   </FormControl>
                   <FormDescription>
-                    A secret to sign the webhook payloads. This is optional but
-                    recommended for security reasons.
+                    A secret to verify the webhook payloads. This is optional
+                    but recommended for security reasons.
                   </FormDescription>
                 </FormItem>
               )}
             />
+
             <div>
-              <span className="text-sm font-medium"> Trigger Events</span>
+              <span className="text-m font-medium"> Trigger Events</span>
             </div>
-            <FormField
-              control={form.control}
-              name="sbomEnabled"
-              render={({ field }) => (
-                <FormItem className="flex flex-row justify-between items-center">
-                  <FormLabel>Enable SBOM Notifications</FormLabel>
-                  <FormControl>
-                    <Switch
-                      checked={field.value}
-                      onCheckedChange={field.onChange as any}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="vulnEnabled"
-              render={({ field }) => (
-                <FormItem className="flex flex-row justify-between items-center">
-                  <FormLabel>Enable Vulnerability Notifications</FormLabel>
-                  <FormControl>
-                    <Switch
-                      checked={field.value}
-                      onCheckedChange={field.onChange as any}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+            <Card>
+              <div className="flex flex-col gap-4 p-2">
+                <FormField
+                  control={form.control}
+                  name="sbomEnabled"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row justify-between items-center">
+                      <FormLabel>Enable SBOM Notifications</FormLabel>
+                      <FormControl>
+                        <Switch
+                          checked={field.value}
+                          onCheckedChange={field.onChange as any}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="vulnEnabled"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row justify-between items-center">
+                      <FormLabel>Enable Vulnerability Notifications</FormLabel>
+                      <FormControl>
+                        <Switch
+                          checked={field.value}
+                          onCheckedChange={field.onChange as any}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </Card>
 
             <div className="flex flex-row justify-end">
               <div className="flex flex-col items-end justify-end gap-2">
@@ -263,10 +301,6 @@ export const WebhookIntegrationDialog: FunctionComponent<Props> = ({
                     </AsyncButton>
                   )}
                 </div>
-                <small className="text-muted-foreground">
-                  Ensure that the URL is publicly accessible and can receive
-                  POST requests from DevGuard.
-                </small>
               </div>
             </div>
           </form>
