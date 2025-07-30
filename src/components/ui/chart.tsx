e07c97a57d72fa377fbe16642db4e2ef9ebb2872
Tim Bastin <bastin.tim@gmail.com>
2025-07-30 18:35:02 +0200

    resolves merge conflicts

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -77,6 +77,7 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   return (
     <style
       dangerouslySetInnerHTML={{
+        // @ts-ignore
         __html: Object.entries(THEMES).map(
           ([theme, prefix]) => `
 ${prefix} [data-chart=${id}] {
