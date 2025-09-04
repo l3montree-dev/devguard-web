effc7f11e0ff9eefd0f4c0261df756dc41ee1f65
Tim Bastin <bastin.tim@gmail.com>
2025-09-04 17:58:57 +0200

    fixes asset title same as project title

--- src/components/common/AssetTitle.tsx
+++ src/components/common/AssetTitle.tsx
@@ -5,7 +5,7 @@ import { useActiveAssetVersion } from "@/hooks/useActiveAssetVersion";
 import Link from "next/link";
 import React from "react";
 import { Badge } from "../ui/badge";
-import { ProjectElement } from "./ProjectTitle";
+import ProjectTitle from "./ProjectTitle";
 
 const AssetTitle = () => {
   const activeOrg = useActiveOrg();
@@ -14,15 +14,15 @@ const AssetTitle = () => {
   const assetVersion = useActiveAssetVersion();
 
   return (
-    <span className="flex flex-row gap-2">
-      <span className="opacity-75">/</span>
-      <ProjectElement project={project} activeOrg={activeOrg} />
-      <span className="opacity-75">/</span>
+    <span className="flex flex-row gap-2 min-w-0 overflow-hidden">
+      <ProjectTitle />
+      <span className="opacity-75 flex-shrink-0">/</span>
       <Link
-        className="flex !text-white items-center gap-1 hover:no-underline"
+        className="flex !text-white items-center gap-1 hover:no-underline min-w-0"
         href={`/${activeOrg?.slug}/projects/${project?.slug}/assets/${asset?.slug}/refs/${assetVersion?.slug}/dependency-risks`}
+        title={asset?.name}
       >
-        {asset?.name}
+        <span className="truncate">{asset?.name}</span>
         <Badge className="!text-white" variant="outline">
           Repository
         </Badge>
