094a4c9774e36d2fb75ecee024d2b04a19a39e13
Tim Bastin <bastin.tim@gmail.com>
2025-10-10 15:47:41 +0200

    fixes type error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -133,7 +133,7 @@ export default async function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <InternalServerErrorPage />
+            <InternalServerErrorPage error={error as Error} />
           </ThemeProvider>
         </body>
       </html>
