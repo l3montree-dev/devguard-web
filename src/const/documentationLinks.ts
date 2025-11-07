5cbe7ff94304f6e116b46bffd0b1bf6e555a14c5
rafi <refaei.shikho@hotmail.com>
2025-11-07 13:49:16 +0100

    Add documentation links for SBOM and VEX in ManualIntegrationSlide
    Signed-off-by: rafi <refaei.shikho@hotmail.com>

---
+++ src/const/documentationLinks.ts
@@ -0,0 +1,7 @@
+// Copyright 2025 L3montree GmbH and the DevGuard Contributors.
+// SPDX-License-Identifier: 	AGPL-3.0-or-later
+export const documentationLinks = {
+  sbomExplaining: "https://devguard.org/concept-guides/explaining-sboms",
+  vexExplaining:
+    "https://devguard.org/concepts/vulnerability-management/joint-vulnerability-management-vex",
+};
