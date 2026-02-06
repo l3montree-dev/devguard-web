f7394a560be95d371f1ae13403022dcf8cbfc5e0
Lars Hermges <lschwan43@gmail.com>
2026-02-06 12:07:06 +0100

    fix reference and additional job

--- src/integrationSnippets.ts
+++ src/integrationSnippets.ts
@@ -356,36 +356,26 @@ export const integrationSnippets = ({
       frontendUrl,
     ),
 
-    sarif: `jobs:
-    code-risk-identification: # what you want to name the job
-        steps:
-            # ...
-            # generate the SARIF file
-            # ...
-            - uses: l3montree-dev/devguard-action/.github/workflows/upload-sarif.yml@main
-              with:
-                  # Path to SARIF file relative to the root of the repository
-                  sarif-file: ./results.sarif
-                  asset-name: "${orgSlug}/projects/${projectSlug}/assets/${assetSlug}"
-                  api-url: "${apiUrl}"
-                  web-ui: "${frontendUrl}"
-              secrets:
-                  devguard-token: "\${{ secrets.DEVGUARD_TOKEN }}" # you need to create`,
-    sbom: `jobs:
-    code-risk-identification: # what you want to name the job
-        steps:
-            # ...
-            # generate the SBOM file
-            # ...
-            - uses: l3montree-dev/devguard-action/.github/workflows/upload-sbom.yml@main
-              with:
-                  # Path to SBOM file relative to the root of the repository
-                  sbom-file: ./results.sbom
-                  asset-name: "${orgSlug}/projects/${projectSlug}/assets/${assetSlug}"
-                  api-url: "${apiUrl}"
-                  web-ui: "${frontendUrl}"
-              secrets:
-                  devguard-token: "\${{ secrets.DEVGUARD_TOKEN }}" # you need to create this secret in your GitHub repository settings`,
+    sarif: `
+    sarif-risk-identification:
+        uses: ./.github/workflows/code-risk-identification.yml
+        with:
+            asset-name: "${orgSlug}/projects/${projectSlug}/assets/${assetSlug}"
+            api-url: "${apiUrl}"
+            sarif-file: "./results.sarif"
+            web-ui: "${frontendUrl}"
+        secrets:
+            devguard-token: \${{ secrets.DEVGUARD_TOKEN }}`,
+    sbom: `
+    sbom-risk-identification:
+        uses: ./.github/workflows/sbom-risk-identification.yml
+        with:
+            asset-name: "${orgSlug}/projects/${projectSlug}/assets/${assetSlug}"
+            api-url: "${apiUrl}"
+            sbom-file: "./results.sbom"
+            web-ui: "${frontendUrl}"
+        secrets:
+            devguard-token: \${{ secrets.DEVGUARD_TOKEN }}`,
     push: "",
     sign: "",
     attest: "",
