c6059aec6cdbb8dccfa9b70b157d6ae729276b42
Lars Hermges <lschwan43@gmail.com>
2026-03-09 13:09:12 +0100

    deb case split added and restyled quick fixes

--- src/components/Quickfix.tsx
+++ src/components/Quickfix.tsx
@@ -32,7 +32,9 @@ function renderQuickFixText(
       return `apk add ${directDependencyFixedVersion}`;
     }
     case "deb": {
-      return `apt-get install -y ${directDependencyFixedVersion}`;
+      const [packageName, packageVersion] =
+        directDependencyFixedVersion.split("@");
+      return `apt-get install -y ${packageName}=${packageVersion}`;
     }
     default:
       return "";
@@ -53,10 +55,15 @@ const Quickfix: FunctionComponent<{ vuln: DetailedDependencyVulnDTO }> = ({
   return (
     <div className="p-5">
       <div className="flex flex-row mb-2 gap-2 items-center">
-        <Zap />
         <h3 className=" text-sm font-semibold">Quick Fix</h3>
+        {/* <Zap /> */}
       </div>
-      <div className="rounded-lg border bg-card p-4 border">
+
+      <div className="relative rounded-lg border bg-card p-4 border">
+        <span className="absolute top-0 right-0 -translate-y-1/2 translate-x-1/2 flex size-3">
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+          <span className="relative inline-flex size-3 rounded-full bg-green-500"></span>
+        </span>
         <div className="text-sm">
           {!directDependencyFixedVersion ? (
             <span className="text-xs text-muted-foreground">
@@ -65,11 +72,9 @@ const Quickfix: FunctionComponent<{ vuln: DetailedDependencyVulnDTO }> = ({
           ) : (
             <>
               <span className="text-xs text-muted-foreground">
-                Update direct dependency to version to fix the vulnerability.{" "}
-                {directDependencyFixedVersion}
+                {`Fix ${vuln.cveID} by updating to:`}
+                <CopyCode codeString={ecosystemUpdate} />
               </span>
-
-              <CopyCode codeString={ecosystemUpdate} />
             </>
           )}
         </div>
