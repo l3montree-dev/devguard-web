e72987851a3fd22830aac84b224527cf077ec781
Tim Bastin <bastin.tim@gmail.com>
2026-04-07 12:22:07 +0200

    testing autoPatchElf

--- nix/nodejs.nix
+++ nix/nodejs.nix
@@ -2,7 +2,9 @@
   # Shared libraries needed by the prebuilt Node.js Linux binaries (dynamically
   # linked against glibc). Include these in the OCI image contents alongside the
   # nodejs derivation so the binary can resolve them at runtime.
-  linuxLibs = targetPkgs: [ targetPkgs.glibc targetPkgs.stdenv.cc.cc.lib ];
+  linuxLibs = targetPkgs:
+    [ targetPkgs.glibc targetPkgs.stdenv.cc.cc.lib ]
+    ++ pkgs.lib.optionals (targetPkgs ? libatomic) [ targetPkgs.libatomic ];
 
   common = {
     installPhase = ''
@@ -11,14 +13,12 @@
     '';
   };
 
-  mkLinuxInstallPhase = { targetPkgs, interpreter }: ''
-    mkdir -p $out
-    cp -r * $out/
-    patchelf \
-      --set-interpreter ${interpreter} \
-      --set-rpath ${pkgs.lib.makeLibraryPath (linuxLibs targetPkgs)} \
-      $out/bin/node
-  '';
+  mkLinuxDerivation = { name, src, targetPkgs }: pkgs.stdenv.mkDerivation {
+    inherit name src;
+    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+    buildInputs = linuxLibs targetPkgs;
+    installPhase = common.installPhase;
+  };
 
   aarch64-darwin = pkgs.stdenv.mkDerivation {
       name = "nodejs-25.9.0-darwin-arm64";
@@ -29,30 +29,22 @@
       inherit (common) installPhase;
   };
 
-  aarch64-linux = pkgs.stdenv.mkDerivation {
+  aarch64-linux = mkLinuxDerivation {
       name = "nodejs-25.9.0-linux-arm64";
       src = pkgs.fetchurl {
         url = "https://nodejs.org/dist/v25.9.0/node-v25.9.0-linux-arm64.tar.gz";
         sha256 = "sha256-j7QoMwG4xyD8nxi///D2WecswU0M8geju0EYCKqnOlc=";
       };
-      nativeBuildInputs = [ pkgs.patchelf ];
-      installPhase = mkLinuxInstallPhase {
-        targetPkgs = pkgsLinuxArm64;
-        interpreter = "${pkgsLinuxArm64.glibc}/lib/ld-linux-aarch64.so.1";
-      };
+      targetPkgs = pkgsLinuxArm64;
   };
 
-  x86_64-linux = pkgs.stdenv.mkDerivation {
+  x86_64-linux = mkLinuxDerivation {
       name = "nodejs-25.9.0-linux-x64";
       src = pkgs.fetchurl {
         url = "https://nodejs.org/dist/v25.9.0/node-v25.9.0-linux-x64.tar.gz";
-        sha256 = pkgs.lib.fakeSha256;
+        sha256 = "sha256-E05VskCESKIZdg/gTcRNaFH53op5VJAh/9hw6Qgtnns=";
       };
-      nativeBuildInputs = [ pkgs.patchelf ];
-      installPhase = mkLinuxInstallPhase {
-        targetPkgs = pkgsLinuxAmd64;
-        interpreter = "${pkgsLinuxAmd64.glibc}/lib/ld-linux-x86-64.so.2";
-      };
+      targetPkgs = pkgsLinuxAmd64;
   };
 
   x86_64-darwin = pkgs.stdenv.mkDerivation {
