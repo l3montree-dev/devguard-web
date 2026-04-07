bb3df00e0038d5ebb1ba418642647dd4b84939ef
Tim Bastin <bastin.tim@gmail.com>
2026-04-07 13:02:20 +0200

    passing interpreter path

--- nix/nodejs.nix
+++ nix/nodejs.nix
@@ -1,10 +1,8 @@
-{ pkgs, pkgsLinuxAmd64 ? pkgs, pkgsLinuxArm64 ? pkgs }: rec {
+{ pkgs, pkgsLinuxAmd64, pkgsLinuxArm64 }: rec {
   # Shared libraries needed by the prebuilt Node.js Linux binaries (dynamically
   # linked against glibc). Include these in the OCI image contents alongside the
   # nodejs derivation so the binary can resolve them at runtime.
-  linuxLibs = targetPkgs:
-    [ targetPkgs.glibc targetPkgs.stdenv.cc.cc.lib ]
-    ++ pkgs.lib.optionals (targetPkgs ? libatomic) [ targetPkgs.libatomic ];
+  linuxLibs = targetPkgs: [ targetPkgs.glibc targetPkgs.stdenv.cc.cc.lib ];
 
   common = {
     installPhase = ''
@@ -13,12 +11,21 @@
     '';
   };
 
-  mkLinuxDerivation = { name, src, targetPkgs }: pkgs.stdenv.mkDerivation {
-    inherit name src;
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = linuxLibs targetPkgs;
-    installPhase = common.installPhase;
-  };
+  # Runs on the host stdenv (Darwin or Linux native), uses patchelf to rewrite
+  # the interpreter and RPATH to point at the target Linux store paths.
+  mkLinuxDerivation = { name, src, targetPkgs, interpreter }:
+    pkgs.stdenv.mkDerivation {
+      inherit name src;
+      nativeBuildInputs = [ pkgs.patchelf ];
+      installPhase = ''
+        mkdir -p $out
+        cp -r * $out/
+        patchelf \
+          --set-interpreter ${interpreter} \
+          --set-rpath ${pkgs.lib.makeLibraryPath (linuxLibs targetPkgs)} \
+          $out/bin/node
+      '';
+    };
 
   aarch64-darwin = pkgs.stdenv.mkDerivation {
       name = "nodejs-25.9.0-darwin-arm64";
@@ -36,6 +43,7 @@
         sha256 = "sha256-j7QoMwG4xyD8nxi///D2WecswU0M8geju0EYCKqnOlc=";
       };
       targetPkgs = pkgsLinuxArm64;
+      interpreter = "${pkgsLinuxArm64.glibc}/lib/ld-linux-aarch64.so.1";
   };
 
   x86_64-linux = mkLinuxDerivation {
@@ -45,6 +53,7 @@
         sha256 = "sha256-E05VskCESKIZdg/gTcRNaFH53op5VJAh/9hw6Qgtnns=";
       };
       targetPkgs = pkgsLinuxAmd64;
+      interpreter = "${pkgsLinuxAmd64.glibc}/lib/ld-linux-x86-64.so.2";
   };
 
   x86_64-darwin = pkgs.stdenv.mkDerivation {
