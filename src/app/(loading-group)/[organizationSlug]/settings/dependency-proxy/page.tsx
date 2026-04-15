bed65a9035a1919332572c31f91b82b16d878b2e
rafi <refaei.shikho@hotmail.com>
2026-04-15 12:12:49 +0200

    add Dependency Proxy settings page and navigation link in org settings
    Signed-off-by: rafi <refaei.shikho@hotmail.com>

---
+++ src/app/(loading-group)/[organizationSlug]/settings/dependency-proxy/page.tsx
@@ -0,0 +1,39 @@
+// Copyright 2026 L3montree GmbH and the DevGuard Contributors.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+"use client";
+
+import ConfigFileEditor from "@/components/common/ConfigFileEditor";
+import DependencyProxySettings from "@/components/common/DependencyProxySettings";
+import Page from "@/components/Page";
+import { useActiveOrg } from "@/hooks/useActiveOrg";
+import { useOrganizationMenu } from "@/hooks/useOrganizationMenu";
+
+const Config = () => {
+  const org = useActiveOrg();
+  const orgMenu = useOrganizationMenu();
+
+  const baseUrl = org ? "/organizations/" + org.slug : null;
+
+  return (
+    <Page
+      breadcrumbs={[
+        {
+          title: "Settings",
+          href: "./",
+        },
+        {
+          title: "Dependency Proxy",
+          href: "",
+        },
+      ]}
+      Title={null}
+      title={""}
+      Menu={orgMenu}
+    >
+      <DependencyProxySettings baseUrl={baseUrl} />
+    </Page>
+  );
+};
+
+export default Config;
--- src/app/(loading-group)/[organizationSlug]/settings/page.tsx
+++ src/app/(loading-group)/[organizationSlug]/settings/page.tsx
@@ -554,6 +554,21 @@ const Home = () => {
           </div>
         </Card>
       </Section>
+      <Section
+        id="dependency-proxy"
+        title="Dependency Proxy Settings"
+        description="View and edit the settings for the Dependency Proxy, which caches dependencies to speed up builds and reduce load on external package registries."
+      >
+        <Card className="p-6">
+          <div className="flex justify-end">
+            <Link href={"/" + activeOrg.slug + "/settings/dependency-proxy"}>
+              <Button variant={"outline"}>
+                Go to Dependency Proxy Settings{" "}
+              </Button>
+            </Link>
+          </div>
+        </Card>
+      </Section>
       <hr />
       <FormProvider {...form}>
         <form onSubmit={form.handleSubmit(handleUpdate)}>
