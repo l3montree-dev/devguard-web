b17650ae210f0ccd283d58b00617a55de34f9279
Julian Kepka <julian.kepka@l3montree.com>
2026-07-06 12:50:35 +0200

    fix: corrected the quick fixes

--- src/utils/view.ts
+++ src/utils/view.ts
@@ -370,6 +370,10 @@ export const reduceRiskHistories = (
         acc.cvePurlMediumCvss += curr.cvePurlMediumCvss;
         acc.cvePurlHighCvss += curr.cvePurlHighCvss;
         acc.cvePurlCriticalCvss += curr.cvePurlCriticalCvss;
+        acc.cvePurlFixableLowCvss += curr.cvePurlFixableLowCvss ?? 0;
+        acc.cvePurlFixableMediumCvss += curr.cvePurlFixableMediumCvss ?? 0;
+        acc.cvePurlFixableHighCvss += curr.cvePurlFixableHighCvss ?? 0;
+        acc.cvePurlFixableCriticalCvss += curr.cvePurlFixableCriticalCvss ?? 0;
         return acc;
       },
       {
@@ -389,6 +393,10 @@ export const reduceRiskHistories = (
         cvePurlMediumCvss: 0,
         cvePurlHighCvss: 0,
         cvePurlCriticalCvss: 0,
+        cvePurlFixableLowCvss: 0,
+        cvePurlFixableMediumCvss: 0,
+        cvePurlFixableHighCvss: 0,
+        cvePurlFixableCriticalCvss: 0,
       } as RiskHistory,
     );
   });
