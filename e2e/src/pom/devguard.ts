1aec44014eb027282759047ce37326f8c47bb370
Julian Kepka <julian.kepka@l3montree.com>
2026-07-27 16:05:42 +0200

    feat: added playwright test for security advisory and screenshots for docu

--- e2e/src/pom/devguard.ts
+++ e2e/src/pom/devguard.ts
@@ -11,6 +11,7 @@ import { VulnFlow } from "./flows/vuln";
 import { ArtifactFlow } from "./flows/artifact";
 import { ShareFlow } from "./flows/sharing";
 import { ModalHelper } from "./flows/modal-helper";
+import { AdvisoryHelper } from "./flows/advisory";
 
 export enum DevGuardNavigationLevel {
   Root = ".level-root",
@@ -61,6 +62,10 @@ export class DevGuardPOM {
 
   modal(): ModalHelper {
     return new ModalHelper(this.page);
+  }
+
+  advisory(): AdvisoryHelper {
+    return new AdvisoryHelper(this.page);
   }
 
   async loadDevGuard() {
---
+++ e2e/src/pom/flows/advisory.ts
@@ -0,0 +1,23 @@
+import type { Page } from "@playwright/test";
+
+export class AdvisoryHelper {
+  constructor(private page: Page) {}
+
+  async createNewAdvisory(title: string, description: string, vector: string, ecosystem: string, packageName: string, semverStart: string, semverEnd: string) {
+    await this.page.getByTestId("title-security-advisory").click();
+    await this.page.getByTestId("title-security-advisory").fill(title);
+    await this.page.getByRole('textbox', { name: 'editable markdown' }).getByRole('paragraph').click();
+    await this.page.getByRole('textbox', { name: 'editable markdown' }).getByRole('paragraph').fill(description);
+    await this.page.getByTestId('vectorString-security-advisory').click();
+    await this.page.getByTestId('vectorString-security-advisory').fill(vector);
+    await this.page.getByTestId("ecosystem-security-advisory").click();
+    await this.page.getByTestId("ecosystem-security-advisory").fill(ecosystem);
+    await this.page.getByTestId("packageName-security-advisory").click();
+    await this.page.getByTestId("packageName-security-advisory").fill(packageName);
+    await this.page.getByTestId("semverStart-security-advisory").click();
+    await this.page.getByTestId("semverStart-security-advisory").fill(semverStart);
+    await this.page.getByTestId("semverEnd-security-advisory").click();
+    await this.page.getByTestId("semverEnd-security-advisory").fill(semverEnd);
+    await this.page.getByTestId("submit-security-advisory").click();
+  }
+}
