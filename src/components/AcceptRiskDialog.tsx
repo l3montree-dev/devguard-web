131257b165d883513e3c57e0aa153e040de34fbf
Dennis Tuan Anh Quach <83472928+Dboy0ZDev@users.noreply.github.com>
2026-07-30 15:06:11 +0200

    Removed redundant prop

--- src/components/AcceptRiskDialog.tsx
+++ src/components/AcceptRiskDialog.tsx
@@ -80,7 +80,6 @@ const AcceptRiskDialog: FunctionComponent<AcceptRiskDialogProps> = ({
               Justification
             </label>
             <MarkdownEditor
-              className="!bg-card"
               placeholder="Add your comment here..."
               value={justification}
               setValue={(value) => setJustification(value ?? "")}
