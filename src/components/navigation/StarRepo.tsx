e757734981183ce0d3850f279cd790bb4855079d
Philipp Schoenbach <philippschoenbach1@gmail.com>
2026-09-16 16:20:35 +0200

    remove hardcoded colors, recreate design with tailwind classes

--- src/components/navigation/StarRepo.tsx
+++ src/components/navigation/StarRepo.tsx
@@ -5,17 +5,17 @@ import { Star } from "lucide-react";
 
 export default function StarRepo() {
   return (
-    <div className="flex h-7 overflow-hidden rounded-md border border-[#d0d7de] bg-[#f1f3f5] text-[#59636e] shadow-sm dark:border-[#3d444d] dark:bg-[#212830] dark:text-[#f0f6fc]">
+    <div className="flex h-7 overflow-hidden rounded-md border border-secondary-foreground/10 bg-secondary">
       <a
         href={`https://github.com/l3montree-dev/devguard`}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center gap-2 px-3 text-sm font-semibold !text-[#59636e] hover:bg-[#e9ecef] hover:no-underline dark:!text-[#f0f6fc] dark:hover:bg-[#2a313c]"
+        className="flex items-center gap-2 px-3 text-sm font-semibold !text-secondary-foreground/65 hover:no-underline hover:bg-secondary-foreground/5"
         aria-label="Star DevGuard on GitHub"
         data-umami-event="Github Star Button Clicked"
       >
-        <Star className="h-4 w-4 text-[#535c66] dark:text-[#9198a1]" />
-        <span>Star</span>
+        <Star className="h-4 w-4" />
+        <span className="dark:text-secondary-foreground">Star</span>
       </a>
     </div>
   );
